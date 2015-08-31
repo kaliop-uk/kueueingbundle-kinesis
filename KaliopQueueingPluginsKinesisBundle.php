0ce0ab31fd1f5bd7b019b316f3b728d0d1e82fc5
gggeek <ggiunta@kaliop.com>
2015-08-31 18:41:48 +0100

    Rename config loader class for proper loading; clean up dead code

--- KaliopQueueingPluginsKinesisBundle.php
+++ KaliopQueueingPluginsKinesisBundle.php
@@ -3,12 +3,7 @@
 namespace Kaliop\Queueing\Plugins\KinesisBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class KaliopQueueingPluginsKinesisBundle extends Bundle
 {
-    /*public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }*/
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Kaliop Queueing Bundle - Kinesis plugin
 
 Adds support for AWS Kinesis to https://github.com/kaliop-uk/kueueingbundle
+
+It has been given its own bundle because it has higher requirements than the base Queueing Bundle
